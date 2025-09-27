16bbade0660b0d3a35bb1cf7167449cc969c9456
sharmayashnarzo <sharmayashnarzo@gmail.com>
2025-09-27 22:41:18 +0530

    Solution for Question 1

--- Question_1.py
+++ Question_1.py
@@ -1,1 +1,11 @@
-print("yash")
+"""Take 2 numbers as input (X and Y) and a third number N.
+ Display all the numbers between X and Y 
+(X < i <= Y) that are divisible by N.
+"""
+X = int(input("Enter the value X : "))
+Y = int(input("Enter the value Y : "))
+N = int(input("Enter the value N : "))
+
+for i in range(X, Y):
+    if(i%N == 0):
+        print(i , " is divisible by " , N)
